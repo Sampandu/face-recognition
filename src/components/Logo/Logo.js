4e7981eda11fd0f63ee254af5ed06dd927d00aa6
Du <Sampan0103314@gmail.com>
2018-12-14 15:47:24 -0500

    styling logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Tilt from 'react-tilt'
+import './Logo.css'
+import face from './face.png'
 
 const Logo = () => {
   return (
-    <div>
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-3" options={{ max : 45 }} style={{ height: 150, width: 150 }}>
+        <div className="Tilt-inner">
+          <img alt='Logo' src={face}/>
+        </div>
+      </Tilt>
     </div>
   )
 }
