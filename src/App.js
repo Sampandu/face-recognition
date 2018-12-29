63cb9b0766a40adcfdc01d8dd94ff8d2f48224bb
Du <Sampan0103314@gmail.com>
2018-12-29 11:11:08 -0500

    add particles.js to background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
@@ -6,10 +7,25 @@ import ImageURLForm from './components/ImageURLForm/ImageURLForm'
 import './App.css';
 import 'tachyons'
 
+const particlesConfig = {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 1000
+      }
+    }
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params = {particlesConfig}
+        />
         <Navigation />
         <Logo />
         <Rank />
