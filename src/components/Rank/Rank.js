d65b03170a5dce449db099383ca2b8b4bc2ce0cf
Du <Sampan0103314@gmail.com>
2019-10-23 13:36:11 -0400

    added one sentence in the rank component for visitors

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Rank = ({ user }) => {
   return (
     <div>
-      <div className='f3 white'>
-        {`${user.name}, your current rank is ...`}
-      </div>
-      <div className='f2 white'>
-        {`${user.submition}`}
-      </div>
+      {Object.keys(user).length === 0 ? (
+        <div>
+          <div className="f3 white">
+            {`${user.name}, your current rank is ...`}
+          </div>
+          <div className="f2 white">{`${user.submition}`}</div>
+        </div>
+      ) : (
+        <div className="f3 white">Hi, welcome to monkey face recognition</div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Rank
+export default Rank;
