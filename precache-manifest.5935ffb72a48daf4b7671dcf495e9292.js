0c3e82189af025cb6d2f29dbf1bfbf809d26b14d
Du <Sampan0103314@gmail.com>
2019-10-23 17:04:09 -0400

    Updates

---
+++ precache-manifest.5935ffb72a48daf4b7671dcf495e9292.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "42e95c951ec0bdaa425bfdb1a9fe654c",
+    "url": "/face-recognition/static/media/face.42e95c95.png"
+  },
+  {
+    "revision": "00a7b039938598a9eb93",
+    "url": "/face-recognition/static/js/runtime~main.00a7b039.js"
+  },
+  {
+    "revision": "9d958747a6de5a0342ba",
+    "url": "/face-recognition/static/js/main.9d958747.chunk.js"
+  },
+  {
+    "revision": "39fbf960dcb2acfd91fc",
+    "url": "/face-recognition/static/js/1.39fbf960.chunk.js"
+  },
+  {
+    "revision": "9d958747a6de5a0342ba",
+    "url": "/face-recognition/static/css/main.a4196468.chunk.css"
+  },
+  {
+    "revision": "39fbf960dcb2acfd91fc",
+    "url": "/face-recognition/static/css/1.29764c55.chunk.css"
+  },
+  {
+    "revision": "9f6c0e95b10b5a50176e7efeea51e2a2",
+    "url": "/face-recognition/index.html"
+  }
+];
